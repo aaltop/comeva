7bbcc91b12ca6361083465fe5cb1be90e0428154
TA <aaltopi@gmail.com>
2025-12-23 18:11:47 +0200

    feat(main)!: Update main to use message validator
    BREAKING-CHANGE: Main changed quite a lot, not going to go through all changes.

--- main.go
+++ main.go
@@ -2,90 +2,16 @@ package main
 
 import (
 	"bufio"
+	bodyValidation "comeva/validators/body"
 	headerValidation "comeva/validators/header"
+	messageValidation "comeva/validators/message"
+	trailerValidation "comeva/validators/trailer"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
-
-// processCommitMessage prints information about invalid formatting
-// in a commit message, and returns a (possibly empty) slice of lines
-// that may contain yaml content: if the commit message contains three dashes,
-// everything after these are assumed (but not tested) to be yaml content.
-func processCommitMessage(scanner *bufio.Scanner) (yamlLines []string) {
-	var line string
-	var possibleYaml bool
-	yamlLines = make([]string, 0)
-	for i := 0; scanner.Scan(); i++ {
-		line = scanner.Text()
-
-		if line == "---" {
-			if possibleYaml {
-				fmt.Println("yaml content should not contain another document start, found one on line", i)
-				continue
-			}
-			fmt.Println("Assumed yaml start on line", i+1)
-			possibleYaml = true
-			continue
-		}
-
-		// yaml content should be at the end of file, so just add the rest
-		// of the lines once the three dashes have been encountered
-		if possibleYaml {
-			yamlLines = append(yamlLines, line)
-			continue
-		}
-
-		switch i {
-		case 0:
-			var err error
-			headerValidator, err := headerValidation.NewHeaderValidator(
-				[]string{"feat", "fix"},
-				[]string{"main", "validators"},
-				[]string{"Add", "Change", "Remove", "Fix"},
-				[2]uint{})
-			if err != nil {
-				panic("HeaderValidator should be valid")
-			}
-			err = headerValidator.ValidateString(line)
-			fmt.Printf("Header: %v\nerror: %v\n", headerValidator.Header, err)
-			fmt.Printf("Header values: %#v\n", headerValidator.Header)
-			// // TODO: do this with a regex
-			// var location, heading, hasLocation = strings.Cut(line, ":")
-			// location = strings.ReplaceAll(location, " ", "")
-			// if !hasLocation {
-			// 	fmt.Println("Warning: no location set for heading")
-			// 	fmt.Printf("Heading: %v\n", location)
-			// } else {
-			// 	heading = strings.Replace(heading, " ", "", 1)
-			// 	fmt.Printf("Location: %v Heading: %v\n", location, heading)
-			// }
-		case 1:
-			if len(line) != 0 {
-				fmt.Println("Line after heading should be empty, found:", line)
-			}
-		}
-
-	}
-	return
-}
-
-// processCommitMessageFile prints information about invalid formatting
-// in a commit message file, and returns a (possibly empty) slice of lines
-// that may contain yaml content: if the commit message contains three dashes,
-// everything after these are assumed (but not tested) to be yaml content.
-func processCommitMessageFile(fileName string) (yamlLines []string) {
-	var file, err = os.Open(fileName)
-	if err != nil {
-		fmt.Printf("Error: Unable to open file: %v\n", err)
-		return
-	}
-	defer file.Close()
-
-	var scanner = bufio.NewScanner(file)
-	yamlLines = processCommitMessage(scanner)
-	return
-}
 
 var flagSet = flag.NewFlagSet("", flag.ExitOnError)
 
@@ -93,30 +19,115 @@ var helpFlag = flagSet.Bool("help", false, "print help")
 var configFile = flagSet.String("config-file", "", "file path for configuration file")
 var commitFile = flagSet.String("commit-file", "", "file path for commit file")
 
-func main() {
+func usageMessage() {
+	var argsOutput = flagSet.Output()
+	fmt.Fprintln(argsOutput, "Usage:")
+	fmt.Fprintln(argsOutput, "  comeva [flags] --commit-file <commit_file>")
+	fmt.Fprintln(argsOutput, "  	Validate the commit message in the file <commit_file>.")
+	fmt.Fprintln(argsOutput, "  comeva [flags] <commit_message>")
+	fmt.Fprintln(argsOutput, "  	Validate the commit message <commit_message>, passed as a string.")
+	fmt.Fprintln(argsOutput, "  comeva --help")
+	fmt.Fprintln(argsOutput, "  	Print help.")
+}
+
+func helpMessage() {
 	var argsOutput = flagSet.Output()
 
-	var usageMessage = func() {
+	fmt.Fprint(argsOutput, "\nCoMeVa (Commit Message Validator) is a tool for validating git commit messages.\n\n")
 
-		fmt.Fprintln(argsOutput, "Usage:")
-		fmt.Fprintln(argsOutput, "  comeva [flags] --commit-file <commit_file>")
-		fmt.Fprintln(argsOutput, "  	Validate the commit message in the file <commit_file>.")
-		fmt.Fprintln(argsOutput, "  comeva [flags] <commit_message>")
-		fmt.Fprintln(argsOutput, "  	Validate the commit message <commit_message>, passed as a string.")
-		fmt.Fprintln(argsOutput, "  comeva --help")
-		fmt.Fprintln(argsOutput, "  	Print help.")
+	usageMessage()
+
+	fmt.Fprintln(argsOutput)
+
+	fmt.Fprint(argsOutput, "Options:\n")
+	flagSet.PrintDefaults()
+}
+
+func getHeaderValidator() (headerValidator *headerValidation.HeaderValidator) {
+	var e error
+	headerValidator, e = headerValidation.NewHeaderValidator(
+		[]string{"feat", "fix"},
+		[]string{"main", "validators"},
+		[]string{"Add", "Change", "Remove", "Update", "Fix"},
+		[2]uint{0, 80})
+	if e != nil {
+		panic(fmt.Sprintf("HeaderValidator should be valid, got error: %v", e))
 	}
+	return headerValidator
+}
 
-	var helpMessage = func() {
-		fmt.Fprint(argsOutput, "\nCoMeVa (Commit Message Validator) is a tool for validating git commit messages.\n\n")
-
-		usageMessage()
-
-		fmt.Fprintln(argsOutput)
-
-		fmt.Fprint(argsOutput, "Options:\n")
-		flagSet.PrintDefaults()
+func getBodyValidator() (bodyValidator *bodyValidation.BodyValidator) {
+	var e error
+	bodyValidator, e = bodyValidation.NewBodyValidator([2]uint{0, 80})
+	if e != nil {
+		panic(fmt.Sprintf("BodyValidator should be valid, got error: %v", e))
 	}
+	return bodyValidator
+}
+
+func getTrailerValidator() (trailerValidator *trailerValidation.TrailerValidator) {
+	var e error
+	var requiredKeys trailerValidation.KeyMap
+	var optionalKeys trailerValidation.KeyMap
+	trailerValidator, e = trailerValidation.NewTrailerValidator(
+		requiredKeys,
+		optionalKeys,
+		2,
+		[2]uint{0, 80},
+	)
+	if e != nil {
+		panic(fmt.Sprintf("TrailerValidator should be valid, got error: %v", e))
+	}
+	return trailerValidator
+}
+
+func getMessageValidator() (messageValidator *messageValidation.MessageValidator) {
+	var e error
+	messageValidator, e = messageValidation.NewMessageValidator(
+		*getHeaderValidator(),
+		*getBodyValidator(),
+		*getTrailerValidator(),
+	)
+	if e != nil {
+		panic(fmt.Sprintf("MessageValidator should be valid, got error: %v\n", e))
+	}
+	return messageValidator
+}
+
+func validateCommitFile(commitFile string) (e error) {
+	var file *os.File
+	file, e = os.Open(commitFile)
+	if e != nil {
+		fmt.Printf("Error opening file '%s': %v\n", commitFile, e)
+		return e
+	}
+	defer file.Close()
+
+	return validateCommitMessage(file)
+}
+
+func validateCommitMessage(reader io.Reader) (e error) {
+	var validator *messageValidation.MessageValidator = getMessageValidator()
+	e = validator.Validate(reader)
+	return e
+}
+
+func printStringLines(str string) {
+	var scanner = bufio.NewScanner(strings.NewReader(str))
+	for i := 1; scanner.Scan(); i++ {
+		fmt.Printf("%4d: %s\n", i, scanner.Text())
+	}
+}
+
+func printCommitMessage(message string) {
+	var delimiterLine = "=================================================="
+	fmt.Println(delimiterLine)
+	printStringLines(message)
+	fmt.Println(delimiterLine)
+}
+
+func main() {
+	var argsOutput = flagSet.Output()
 
 	flagSet.Usage = usageMessage
 	flagSet.Parse(os.Args[1:])
@@ -124,6 +135,8 @@ func main() {
 	var args []string = flagSet.Args()
 	var commitFileSpecified = len(*commitFile) > 0
 	var numArgs = len(args)
+
+	// check validity of input
 	switch {
 	case *helpFlag:
 		helpMessage()
@@ -138,17 +151,27 @@ func main() {
 		return
 	}
 
+	var e error
 	if numArgs == 1 {
 		var commitMessage string = args[0]
-		fmt.Printf("Message:\n%s\n", commitMessage)
+		printCommitMessage(commitMessage)
+		e = validateCommitMessage(strings.NewReader(commitMessage))
+		if e != nil {
+			fmt.Printf("Found issues validating message:\n%v\n", e)
+		} else {
+			fmt.Println("No issues found validating message.")
+		}
+
+	} else {
+		e = validateCommitFile(*commitFile)
+		if e != nil {
+			fmt.Printf("Found issues validating file '%s':\n%v\n", *commitFile, e)
+		} else {
+			fmt.Printf("No issues found with file '%s'\n", *commitFile)
+		}
 	}
 
-	if len(*commitFile) < 1 {
-		return
-	}
-	var yamlLines []string = processCommitMessageFile(*commitFile)
-	fmt.Println("yaml:")
-	for _, yamlLine := range yamlLines {
-		fmt.Println(yamlLine)
+	if e != nil {
+		os.Exit(1)
 	}
 }
