c3f023f65cfc17f63a4fd53463599a4036b21515
TA <aaltopi@gmail.com>
2026-05-14 14:18:02 +0300

    feat: Implement ColoredLogger
    Effect-Dev: external
    Effect-UI: external

--- internal/logging/utils.go
+++ internal/logging/utils.go
@@ -1,8 +1,41 @@
 package logging
 
+import (
+	"comeva/internal/io"
+	"comeva/internal/io/ansi"
+	baseIo "io"
+)
+
 // CreateColoredLogger creates a coloured logger which has suitable coloured
 // output for each logging level.
 func CreateColoredLogger() (logger *LevelLogger) {
-	// TODO: actually implement this
-	return NewLevelLoggerWithDefaults()
+
+	logger = NewLevelLoggerWithDefaults()
+	var createModWriter = func(colorScheme *ansi.ColorScheme) baseIo.Writer {
+		var modifier = io.NewDefaultModifier[string]()
+		modifier.Add(colorScheme.ApplyFore)
+		return io.NewStringModifierWriter(
+			modifier,
+			logger.baseOut,
+		)
+	}
+
+	logger.Configure(LevelConfigs{
+		Debug: &LevelConfig{
+			Out: createModWriter(ansi.BasicColorSchemes.Debug),
+		},
+		Info: &LevelConfig{
+			Out: createModWriter(ansi.BasicColorSchemes.Info),
+		},
+		Warning: &LevelConfig{
+			Out: createModWriter(ansi.BasicColorSchemes.Warning),
+		},
+		Error: &LevelConfig{
+			Out: createModWriter(ansi.BasicColorSchemes.Error),
+		},
+		Critical: &LevelConfig{
+			Out: createModWriter(ansi.BasicColorSchemes.Critical),
+		},
+	})
+	return
 }
